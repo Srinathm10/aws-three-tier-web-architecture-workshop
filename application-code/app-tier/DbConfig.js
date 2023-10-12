bb27bef027181464ff642a425212475729892b55
Srinathm10 <109588493+Srinathm10@users.noreply.github.com>
2023-10-11 23:34:11 -0400

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1.cluster-cnftcbdukyv2.ca-central-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Basinsei',
+    DB_DATABASE : 'webappdb'
 });
